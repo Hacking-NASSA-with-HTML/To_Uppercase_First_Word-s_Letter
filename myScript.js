106c453b5e9dd49ffdbc8558fe83f34b1df0c203
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-07-29 17:17:34 +0400

    feat: add ready script file

--- myScript.js
+++ myScript.js
@@ -1,1 +1,10 @@
-console.log('hi!')
+// console.log('Hi, Overlord!!!')
+
+function capitalizeFirstLetterOfTheWord(string) {
+    const arrayFromString = string.split('')
+    arrayFromString[0] = arrayFromString[0].toUpperCase()
+    return arrayFromString.join('')
+}
+
+
+console.log(capitalizeFirstLetterOfTheWord('victory'))
